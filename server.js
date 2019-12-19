8d82e9e22f41ae016e497956ebf0d81f361329b4
Bailey Garrett <baileyagarrett@gmail.com>
2019-12-18 20:13:33 -0600

    adjusting paths

--- server.js
+++ server.js
@@ -43,11 +43,11 @@ var characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "make.html"));
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 // Displays all characters
