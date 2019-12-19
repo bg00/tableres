fb397435d87ea473429d365ffa1f3ed876d0dbac
Bailey Garrett <baileyagarrett@gmail.com>
2019-12-18 20:15:20 -0600

    adjusting paths

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
